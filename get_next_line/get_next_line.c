e6e2572433f49950ddbe67411ef03deb070f783c
pineauofficial <117824819+pineauofficial@users.noreply.github.com>
2022-11-30 19:16:25 +0100

    Add files via upload

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pineau <pineau@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/30 18:18:53 by pineau            #+#    #+#             */
+/*   Updated: 2022/11/30 19:15:00 by pineau           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_check(void *buf)
+{
+	int	a;
+
+	a = 0;
+	while (buf)
+	{
+		if(buf[a] = '$')
+			return (0);
+	}
+	return (1);
+}
+
+char	*get_next_line(int fd)
+{
+	void *buf;
+	
+	buf = malloc(sizeof(char) * nbytes)
+	if(!buf)
+		return (NULL)
+	while (ft_check(buf) != 0)
+	{
+		read(int fd, void *buf, size_t nbytes)
+	}
+}
