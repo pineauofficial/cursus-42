f6967b2822c2460795946933bd3b826cd3fcfbfa
pineauofficial <117824819+pineauofficial@users.noreply.github.com>
2022-12-10 16:32:41 +0100

    Add files via upload

--- push_swap/push_swap_list_maker.c
+++ push_swap/push_swap_list_maker.c
@@ -6,7 +6,7 @@
 /*   By: pineau <pineau@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/10 14:54:36 by pineau            #+#    #+#             */
-/*   Updated: 2022/12/10 14:55:14 by pineau           ###   ########.fr       */
+/*   Updated: 2022/12/10 16:30:04 by pineau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,15 +71,38 @@ void	free_list(t_list *head)
 	}
 }
 
-int	main(void)
+int	ft_atoi(const char *nptr)
+{
+	char	*str;
+	int		nbr;
+	int		i;
+	int		signe;
+
+	i = 0;
+	str = (char *)nptr;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	signe = 1;
+	if (str[i] == '+' || str[i] == '-')
+		if (str[i++] == '-')
+			signe = -signe;
+	nbr = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+		nbr = nbr * 10 + (str[i++] - '0');
+	return (nbr * signe);
+}
+
+int	main(int argc, char **argv)
 {
 	t_list	*head;
+	int		a;
 
-	head = make_nod(0);
-	add_nod(head, make_nod(1));
-	add_nod(head, make_nod(2));
-	add_nod(head, make_nod(3));
-	add_nod(head, make_nod(4));
+	if (argc < 2)
+		return (0);
+	a = 1;
+	head = make_nod(ft_atoi(argv[a]));
+	while (++a < argc)
+		add_nod(head, make_nod(ft_atoi(argv[a])));
 	display_list(head);
 	free_list(head);
 	return (0);
